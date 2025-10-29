a3a25cae60c9bb06e814698c85cf36f4893ab8ad
nansvanidze-png <nansvanidze@gmail.com>
2025-10-30 03:22:58 +0400

    Add files via upload

--- assigment.js
+++ assigment.js
@@ -1,8 +1,29 @@
-/*let country = "georgia";
-let continent = "europe";
-let population = "4";
+const day = "monday";
 
-console.log(country);
-console.log(continent);
-console.log(population)
-*/
+switch (day) {
+    case "monday":
+        console.log("plan curse structure");
+        console.log("go to coding meetup");
+        break;
+    case "tuesday":
+        console.log("prepare theory vidios");
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log("write code examples");
+        break;
+    case "friday":
+        console.log("record videos");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("enjoy the weekends :)");
+        break;
+    default:
+        console.log("not a valid day");
+
+}
+const today = "thursday";
+if ('wednesday' || 'thursday') {
+    console.log("write code examples")
+}
