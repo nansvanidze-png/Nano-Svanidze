9ddcb09469d905ae046a6a4ead6748a18c9c2aa9
nansvanidze-png <nansvanidze@gmail.com>
2025-12-03 23:13:36 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,234 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415
+
+let myFirstJob = "coder";
+let myCurrentJob = "teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob)
+
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun);
+*/
+
+//let age = 30;
+//age = 31;
+
+//const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+// var job = "programmer";
+// job = "teacher";
+// math operators
+//const now = 2037
+//const ageJonas = now - 1991;
+//const ageSarah = now - 2018;
+
+//console.log(ageJonas, ageSarah);
+
+//console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 
+
+//const firstName = "jonas";
+//const lastName = "svanidze";
+
+//console.log(firstName + " " + lastName);
+
+//assignment operators
+//let x = 10 + 5;
+//x += 10; // x = x + 10
+//x *= 4;
+//x++;
+//x--;
+//x--;
+
+//console.log(x);
+
+//comperason operators
+//console.log(ageJonas > ageSarah); // >, <, >=, <=;
+//console.log(ageSarah >= 18);
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah);
+console.log(averageAge)
+*/
+
+/*const firstName = "jonas";
+const job = "teacher";
+const birthYear = "1991";
+const year = 2037;
+
+const jonas = "I'm" + " " + firstName + ', a ' + (year - birthYear) + " " + "year old" + " " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`just a regular string....`);
+
+console.log('string with \n\
+multiple \n\
+lines');
+
+console.log(`string
+multiple
+lines`);
+*/
+/*
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) { console.log('sarah can start driving license 🚗') } else {
+    const yearsLeft = 18 - age;
+    console.log(`sarah is too young. wait another ${yearsLeft} years :)`)
+};
+
+const birthYear = 1998;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century)
+*/
+
+/*
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('jonas'));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+    console.log("don't spend it all ;)");
+} else {
+    console.log("you should get a job!");
+}
+
+let height;
+if (height) {
+    console.log('YAY! height is defined ;)');
+} else {
+    console.log('height is UNDEFINED!');
+}
+
+const age = 18
+if (age === 18) console.log("you just became an adult");
+
+const favourite = Number(prompt("what's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 4) {
+    console.log('cool! 4 is an amazing number!')
+} else if (favourite === 7) {
+    console.log("7 is also cool number")
+} else if (favourite === 9) {
+    console.log('9 is also cool number')
+} else { console.log('number is not 4 or 7 or 9') }
+
+if (favourite !== 4) console.log('why not 4?');
+*/
+
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+// if (shouldDrive) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+// const isTired = false; // C
+// console.log(hasDriversLicense || hasGoodVision || isTired);
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+// const day = "monday";
+
+// switch (day) {
+//     case "monday":
+//         console.log("plan curse structure");
+//         console.log("go to coding meetup");
+//         break;
+//     case "tuesday":
+//         console.log("prepare theory vidios");
+//         break;
+//     case "wednesday":
+//     case "thursday":
+//         console.log("write code examples");
+//         break;
+//     case "friday":
+//         console.log("record videos");
+//         break;
+//     case "saturday":
+//     case "sunday":
+//         console.log("enjoy the weekends :)");
+//         break;
+//     default:
+//         console.log("not a valid day");
+
+// }
+// const today = "thursday";
+// if ('wednesday' || 'thursday') {
+//     console.log("write code examples")
+// }
+
+//const age = 23;
+//age >= 18 ? console.log("i would like to drink wine") :
+//    console.log("i would like to drink water");
+// 
+/*
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+*/
+// console.log(`i like to drink ${age >= 18 ? 'wine' : 'water'}`);
